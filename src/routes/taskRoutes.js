dc985ecb50063bb01c70719d45b5f0401e7ce23a
João Pedro Telini <joaotelini03@gmail.com>
2025-06-18 23:25:40 -0300

    refactor: replace editSTask with editStatusTaskController in taskRoutes

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -2,21 +2,26 @@ import express from "express";
 import {
   getTasks,
   createTask,
-  editSTask,
   deleteTaskbyId,
+  editStatusTaskController,
 } from "../controller/taskController.js";
 
 import {
   sendTaskMiddleware,
-  verifyIdMiddleware,
   editTaskMiddleware,
+  verifyIdMiddleware,
 } from "../middleware/taskMiddleware.js";
 
 const router = express.Router();
 
 router.get("/", getTasks);
-router.put("/:id", verifyIdMiddleware, editTaskMiddleware, editSTask);
 router.post("/", sendTaskMiddleware, createTask);
+router.put(
+  "/:id",
+  verifyIdMiddleware,
+  editTaskMiddleware,
+  editStatusTaskController
+);
 router.delete("/:id", verifyIdMiddleware, deleteTaskbyId);
 
 export default router;
