ff21175d3de4dbb1f207de588944000c886bf55b
João Pedro Telini <joaotelini03@gmail.com>
2025-06-25 22:28:32 -0300

    refactor: update task model functions to include userId for task operations

--- src/model/taskModel.js
+++ src/model/taskModel.js
@@ -1,26 +1,38 @@
 import { getDataBase } from "../config/mongoConnection.js";
 import { ObjectId } from "mongodb";
 
-export const getAllTasks = async () => {
+export const getAllTasks = async (userId) => {
   const db = getDataBase();
 
-  return await db.collection("tasks").find().toArray();
+  return await db
+    .collection("tasks")
+    .find({ user_id: new ObjectId(String(userId)) })
+    .toArray();
 };
 
-export const editStatusTask = async (id, status) => {
+export const editStatusTask = async (id, userId, status) => {
   const db = getDataBase();
   const collection = db.collection("tasks");
-  const filter = { _id: new ObjectId(String(id)) };
-  const updateStatus = { $set: { status } };
+
+  const filter = {
+    _id: new ObjectId(String(id)),
+    user_id: new ObjectId(String(userId)),
+  };
+
+  const updateStatus = {
+    $set: { status },
+  };
+
   const result = await collection.updateOne(filter, updateStatus);
 
   return result;
 };
 
-export const createTask = async (title) => {
+export const createTask = async (title, userId) => {
   const db = getDataBase();
   const doc = {
     title,
+    user_id: new ObjectId(String(userId)),
     status: false,
   };
 
@@ -29,11 +41,14 @@ export const createTask = async (title) => {
   return result.insertedId;
 };
 
-export const deleteTask = async (id) => {
+export const deleteTask = async (id, userId) => {
   const db = getDataBase();
   const result = await db
     .collection("tasks")
-    .deleteOne({ _id: new ObjectId(String(id)) });
+    .deleteOne({
+      _id: new ObjectId(String(id)),
+      user_id: new ObjectId(String(userId)),
+    });
 
   return result.deletedCount;
 };
