86636a45b46597edeb2fbb478010722485ffc6b0
João Pedro Telini <joaotelini03@gmail.com>
2025-06-26 22:57:16 -0300

    refactor: configure CORS middleware with specific origin and credentials

--- src/server.js
+++ src/server.js
@@ -3,12 +3,19 @@ import cors from "cors";
 import "dotenv/config";
 import taskRouter from "./routes/taskRoutes.js";
 import authRouter from "./routes/authRoutes.js";
+import cookieParser from "cookie-parser";
 
 const port = process.env.PORT || 3333;
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+app.use(cookieParser());
 
 app.use("/tasks", taskRouter);
 app.use("/auth", authRouter);
